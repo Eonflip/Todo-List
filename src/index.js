bb03e7d0dd1b5a9d8421157176c589426c29515a
Ian Jones <ianj1993@gmail.com>
2024-08-17 22:34:15 -0600

    Menu button should now toggle class and build sidebar properly

--- src/index.js
+++ src/index.js
@@ -2,16 +2,71 @@ import createElement from './modules/element';
 import './styles/main.scss'
 
 const header = document.getElementById('header');
-const body = document.getElementById('body');
+const main = document.getElementById('main');
 const footer = document.getElementById('footer');
+const content = document.getElementById('content');
 
 
 //createElement({ tag, className, src, content, parent, attributes })
+
+const menuButton = createElement({
+    tag: 'button',
+    className: 'menu-button',
+    parent: header,
+});
+
+createElement({
+    tag: 'i',
+    className: 'fa-solid fa-bars',
+    parent: menuButton,
+});
+
 createElement({
     tag: 'h1',
     className: 'app-title',
-    src: '',
     content: 'Todoloo',
-    parent: header,
+    parent: header
+});
+
+const sidebar = createElement({
+    tag: 'aside',
+    className: 'sidebar',
+    parent: content,
+});
+
+createElement({
+    tag: 'h2',
+    className: 'sidebar-title',
+    content: 'Sidebar Content',
+    parent: sidebar
+});
+
+const sidebarItems = ['Home', 'Projects', 'Tasks', 'Settings'];
+const sidebarList = createElement({
+    tag: 'ul',
+    className: 'sidebar-list',
+    parent: sidebar
+});
+
+sidebarItems.forEach(item => {
+    createElement({
+        tag: 'li',
+        className: 'sidebar-item',
+        content: item,
+        parent: sidebarList
+    })
+})
+
+createElement({
+    tag: 'h3',
+    className: 'footer-info',
+    content: "Ian Jones Copyright® " + new Date().getFullYear(),
+    parent: footer,
     attributes: {}
 });
+
+
+menuButton.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+    document.body.classList.toggle('sidebar-active');
+})
