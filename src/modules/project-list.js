62be122a6d865ff45dd3decab6abe803169eb001
Ian Jones <ianj1993@gmail.com>
2024-08-21 17:06:55 -0600

    Added methods to add and remove projects

--- src/modules/project-list.js
+++ src/modules/project-list.js
@@ -1,5 +1,19 @@
 export default class ProjectList {
     constructor() {
-        
+        this.projects = [];
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+    }
+
+    removeProject(project) {
+        if (this.projects.includes(project)) {
+            let index = this.projects.indexOf(project);
+            this.projects.splice(index, 1);
+        }
+        else {
+            return null;
+        }
     }
 }
