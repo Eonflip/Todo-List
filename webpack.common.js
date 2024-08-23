127fccd4d309387e532221ed4f43bea1808fe054
Ian Jones <ianj1993@gmail.com>
2024-08-22 18:13:24 -0600

    Updated webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,35 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Todoloo',
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-  ],
+  mode: 'development',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  devServer: {
+    static: './dist',
+    open: true, // Automatically open the browser
+    hot: true,  // Enable Hot Module Replacement
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        }, 
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-        {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
